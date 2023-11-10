48e72e342b92a6744d5940d90291f35caa894505
kaonashiiiiiii <priora102003@gmail.com>
2023-11-10 02:51:14 +0200

    sum

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,66 @@
-const key = new Key();
-
-const house = new MyHouse(key);
-const person = new Person(key);
-
-house.openDoor(person.getKey());
-
-house.comeIn(person);
-
-
-export {};
+class Key {
+    private signature: number;
+  
+    constructor() {
+      this.signature = Math.random();
+    }
+  
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+  
+  class Person {
+    private key: Key;
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+  
+  abstract class House {
+    private door: boolean = false;
+    protected  key: Key;
+    private tenants: Person[] = [];
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    comeIn(person: Person): void {
+      if (this.door) {
+        this.tenants.push(person);
+        console.log('Person entered the house.');
+      } else {
+        console.log('The door is closed. Person cannot enter.');
+      }
+    }
+  
+    abstract openDoor(key: Key): void;
+  }
+  
+  class MyHouse extends House {
+    openDoor(key: Key): void {
+      if (key.getSignature() === this.key.getSignature()) {
+        this['door'] = true; 
+        console.log('The door is opened.');
+      } else {
+        console.log('Invalid key. The door remains closed.');
+      }
+    }
+  }
+  
+  const key = new Key();
+  const house = new MyHouse(key);
+  const person = new Person(key);
+  
+  house.openDoor(person.getKey());
+  house.comeIn(person);
+  
+  console.log('Мешканці будинку:', house['tenants']); 
+  
+  export {};
